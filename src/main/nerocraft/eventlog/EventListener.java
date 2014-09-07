410a3d109ca6358eed7b462478e8cef95e01e2b9
Neroren <oreniouchavaev@gmail.com>
2014-09-07 05:07:12 -0500

    Added functionality
    Damage dealt to players is now logged

--- src/main/nerocraft/eventlog/EventListener.java
+++ src/main/nerocraft/eventlog/EventListener.java
@@ -1,7 +1,33 @@
 ﻿// Copyright © Oren Iouchavaev (Neroren) - Nerocraft
 package main.nerocraft.eventlog;
 
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class EventListener implements Listener {
+    Util util = new Util();
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.isCancelled()) return;
+        if (event.getEntity() instanceof Player) {
+            Player victim = (Player) event.getEntity();
+            EntityType damagerType = event.getDamager().getType();
+            String damager = damagerType.toString();
+            double damage = event.getDamage();
+            if (util.isNPC(victim)) return;
+            if (damagerType == EntityType.PLAYER) {
+                if (util.isNPC((Player) event.getDamager())) return;
+                damager = ((Player) event.getDamager()).getName();
+            }
+            Bukkit.getLogger().log(Level.INFO, victim.getName() + " was damaged by " + damager + " for " + Double.toString(damage) + " health (" + victim.getHealth() + "/" + victim.getMaxHealth() + " Health)");
+        }
+    }
 }
---
+++ src/main/nerocraft/eventlog/Util.java
@@ -0,0 +1,15 @@
+﻿package main.nerocraft.eventlog;
+
+import org.bukkit.entity.Player;
+
+public class Util {
+    /**
+     * Gets whether or not this player is an NPC
+     *
+     * @param player the player
+     * @return true if player is an NPC, otherwise false
+     */
+    public boolean isNPC(Player player) {
+        return player.hasMetadata("NPC");
+    }
+}
